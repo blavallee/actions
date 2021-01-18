ecd35114f0e6fd8f5f98de55f544cedd311009a6
Benoit Lavallee <blavallee@coveo.com>
2021-01-18 17:09:58 -0500

    Error on diff

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt-get update \
     && apt-get install --no-install-recommends --assume-yes \
        clang-format \
        git \
+       colordiff \
     && rm -rf /var/lib/apt/lists/*
 
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -l
 
-pwd
-ls -al
+diff=`git-clang-format HEAD~1 --extensions cpp,c,h,hpp --diff`
+[ "$diff" = "no modified files to format" ] && exit 0
+[ "$diff" = "clang-format did not modify any files" ] && exit 0
 
-git-clang-format HEAD~1 --extensions cpp,c,h,hpp --diff
+printf "\033[1mYou have introduced coding style breakages, suggested changes:\n\n"
+
+echo "$diff" | colordiff
+exit 1
