5238c39ffe27e2eb881fb9fb83cf9bbef42b2e9d
Benoit Lavallee <blavallee@coveo.com>
2021-01-18 17:34:06 -0500

    Make diff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh -l
 
-diff=`git-clang-format HEAD~1 --extensions cpp,c,h,hpp --diff`
-[ "$diff" = "no modified files to format" ] && exit 0
-[ "$diff" = "clang-format did not modify any files" ] && exit 0
-
-printf "\033[1mYou have introduced coding style breakages, suggested changes:\n\n"
-
-echo "$diff" | colordiff
-exit 1
+git-clang-format HEAD^ --extensions cpp,c,h,hpp
