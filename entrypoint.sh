f22b75425a346beb2a6dc8a88691d3cca68a7275
Benoit Lavallee <blavallee@coveo.com>
2021-01-21 13:56:04 -0500

    Add output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
 git clang-format HEAD~
+if [ -n "$(git status --porcelain)" ]; then
+  echo "::set-output name=changes_detected::true";
+else
+  echo "::set-output name=changes_detected::false";
+fi
