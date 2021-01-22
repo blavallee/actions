cadffbbae241db8d397b30e195f18d12a4a7271c
Benoit Lavallee <blavallee@coveo.com>
2021-01-22 09:39:20 -0500

    Test with commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,12 @@
 git clang-format HEAD~
 if [ -n "$(git status --porcelain)" ]; then
   echo "::set-output name=changes_detected::true";
+  git checkout -b ClangFormat-$(git rev-parse HEAD)
+  git config user.name blavallee
+  git config user.email benoitlavallee@gmail.com
+  git commit --all --message "Clang-format" --message "$(git log -1 --pretty=%B)"
+  git push origin ClangFormat-$(git rev-parse HEAD)
 else
   echo "::set-output name=changes_detected::false";
 fi
-ls -al
+
